f9bfb62021312df790dd08defefd1df49354b5b6
Verubato <118912121+Verubato@users.noreply.github.com>
2026-01-05 08:44:18 +1100

    Basic addon working.

---
+++ src/Config.lua
@@ -0,0 +1,270 @@
+local addonName, addon = ...
+local verticalSpacing = 20
+local checkboxesPerLine = 4
+local checkboxWidth = 150
+---@type DB
+local db
+---@type CharDB
+local charDb
+---@class DB
+local dbDefaults = {
+	PlayerRestLoop = true,
+	PrestigeBadges = true,
+	PlayerPortraitCornerIcon = true,
+	PlayerLevelText = true,
+	CompactPartyFrameTitle = true,
+	CompactArenaFrameTitle = true,
+	QuickJoinToastButton = false,
+}
+---@class CharDB
+local charDbDefaults = {
+	StanceBar = false,
+	HotKeysText = false,
+}
+local M = {}
+addon.Config = M
+
+local function CopyTable(src, dst)
+	if type(dst) ~= "table" then
+		dst = {}
+	end
+
+	for k, v in pairs(src) do
+		if type(v) == "table" then
+			dst[k] = CopyTable(v, dst[k])
+		elseif dst[k] == nil then
+			dst[k] = v
+		end
+	end
+
+	return dst
+end
+
+local function AddCategory(panel)
+	if Settings then
+		local category = Settings.RegisterCanvasLayoutCategory(panel, panel.name)
+		Settings.RegisterAddOnCategory(category)
+
+		return category
+	elseif InterfaceOptions_AddCategory then
+		InterfaceOptions_AddCategory(panel)
+
+		return panel
+	end
+
+	return nil
+end
+
+local function CreateSettingCheckbox(panel, setting)
+	local checkbox = CreateFrame("CheckButton", nil, panel, "UICheckButtonTemplate")
+	checkbox.Text:SetText(" " .. setting.Name)
+	checkbox.Text:SetFontObject("GameFontNormal")
+	checkbox:SetChecked(setting.Enabled())
+	checkbox:HookScript("OnClick", function()
+		setting.OnChanged(checkbox:GetChecked())
+	end)
+
+	checkbox:SetScript("OnEnter", function(self)
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+		GameTooltip:SetText(setting.Name, 1, 0.82, 0)
+		GameTooltip:AddLine(setting.Tooltip, 1, 1, 1, true)
+		GameTooltip:Show()
+	end)
+
+	checkbox:SetScript("OnLeave", function()
+		GameTooltip:Hide()
+	end)
+
+	return checkbox
+end
+
+local function LayoutSettings(settings, panel, relativeTo, xOffset, yOffset)
+	local x = xOffset
+	local y = yOffset
+	local bottomLeftCheckbopx = nil
+
+	for i, setting in ipairs(settings) do
+		local checkbox = CreateSettingCheckbox(panel, setting)
+		checkbox:SetPoint("TOPLEFT", relativeTo, "TOPLEFT", x, y)
+
+		if i == 0 or i % (checkboxesPerLine + 1) == 0 then
+			bottomLeftCheckbopx = checkbox
+		end
+
+		if i % checkboxesPerLine == 0 then
+			y = y - (verticalSpacing * 2)
+			x = xOffset
+		else
+			x = x + checkboxWidth
+		end
+	end
+
+	return bottomLeftCheckbopx
+end
+
+function CanOpenOptionsDuringCombat()
+	if LE_EXPANSION_LEVEL_CURRENT == nil or LE_EXPANSION_MIDNIGHT == nil then
+		return true
+	end
+
+	return LE_EXPANSION_LEVEL_CURRENT < LE_EXPANSION_MIDNIGHT
+end
+
+function M:Init()
+	MiniHiderDB = MiniHiderDB or {}
+	MiniHiderCharDB = MiniHiderCharDB or {}
+
+	db = CopyTable(dbDefaults, MiniHiderDB)
+	charDb = CopyTable(charDbDefaults, MiniHiderCharDB)
+
+	local panel = CreateFrame("Frame")
+	panel.name = addonName
+
+	local category = AddCategory(panel)
+
+	if not category then
+		return
+	end
+
+	local version = C_AddOns.GetAddOnMetadata(addonName, "Version")
+	local title = panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+	title:SetPoint("TOPLEFT", 0, -verticalSpacing)
+	title:SetText(string.format("%s - %s", addonName, version))
+
+	local description = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
+	description:SetPoint("TOPLEFT", title, 0, -verticalSpacing)
+	description:SetText("Hide various frames for a cleaner UI.")
+
+	local settings = {
+		{
+			Name = "Resting animation",
+			Tooltip = "Hides the playing 'zzz' animation loop.",
+			Enabled = function()
+				return db.PlayerRestLoop
+			end,
+			OnChanged = function(enabled)
+				db.PlayerRestLoop = enabled
+				addon:Run()
+			end,
+		},
+		{
+			Name = "Prestidge badges",
+			Tooltip = "Hides the player, target, and focus prestige badges.",
+			Enabled = function()
+				return db.PrestigeBadges
+			end,
+			OnChanged = function(enabled)
+				db.PrestigeBadges = enabled
+				addon:Run()
+			end,
+		},
+		{
+			Name = "Player corner icon",
+			Tooltip = "Hides the player portrait bottom right corner icon.",
+			Enabled = function()
+				return db.PlayerPortraitCornerIcon
+			end,
+			OnChanged = function(enabled)
+				db.PlayerPortraitCornerIcon = enabled
+				addon:Run()
+			end,
+		},
+		{
+			Name = "Player level text",
+			Tooltip = "Hides the player portrait level text.",
+			Enabled = function()
+				return db.PlayerLevelText
+			end,
+			OnChanged = function(enabled)
+				db.PlayerLevelText = enabled
+				addon:Run()
+			end,
+		},
+		{
+			Name = "Party title",
+			Tooltip = "Hides the party frames title.",
+			Enabled = function()
+				return db.CompactPartyFrameTitle
+			end,
+			OnChanged = function(enabled)
+				db.CompactPartyFrameTitle = enabled
+				addon:Run()
+			end,
+		},
+		{
+			Name = "Arena title",
+			Tooltip = "Hides the arena frames title.",
+			Enabled = function()
+				return db.CompactArenaFrameTitle
+			end,
+			OnChanged = function(enabled)
+				db.CompactArenaFrameTitle = enabled
+				addon:Run()
+			end,
+		},
+		{
+			Name = "Social icon",
+			Tooltip = "Hides the social icon (a.k.a quick join toast button) above the chat window.",
+			Enabled = function()
+				return db.QuickJoinToastButton
+			end,
+			OnChanged = function(enabled)
+				db.QuickJoinToastButton = enabled
+				addon:Run()
+			end,
+		},
+	}
+
+	local globalHeading = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
+	globalHeading:SetPoint("TOPLEFT", description, 0, -verticalSpacing * 2)
+	globalHeading:SetText("Global settings:")
+
+	local anchor = LayoutSettings(settings, panel, globalHeading, 0, -verticalSpacing)
+
+	local charHeading = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
+	charHeading:SetPoint("TOPLEFT", anchor, 0, -verticalSpacing * 3)
+	charHeading:SetText("Character settings:")
+
+	local charSettings = {
+		{
+			Name = "Stance Bar",
+			Tooltip = "Hides the stance bar (druid forms, warrior stances).",
+			Enabled = function()
+				return charDb.StanceBar
+			end,
+			OnChanged = function(enabled)
+				charDb.StanceBar = enabled
+				addon:Run()
+			end,
+		},
+		{
+			Name = "HotKeys Text",
+			Tooltip = "Hides the hot keys text on your action bars.",
+			Enabled = function()
+				return charDb.HotKeysText
+			end,
+			OnChanged = function(enabled)
+				charDb.HotKeysText = enabled
+				addon:Run()
+			end,
+		},
+	}
+
+	LayoutSettings(charSettings, panel, charHeading, 0, -verticalSpacing)
+
+	SLASH_MINIHIDER1 = "/minihider"
+	SLASH_MINIHIDER2 = "/mh"
+
+	SlashCmdList.MINIHIDER = function()
+		if Settings then
+			if not InCombatLockdown() or CanOpenOptionsDuringCombat() then
+				Settings.OpenToCategory(category:GetID())
+			end
+		elseif InterfaceOptionsFrame_OpenToCategory then
+			-- workaround the classic bug where the first call opens the Game interface
+			-- and a second call is required
+			InterfaceOptionsFrame_OpenToCategory(panel)
+			InterfaceOptionsFrame_OpenToCategory(panel)
+		end
+	end
+end
---
+++ src/MiniHider.lua
@@ -0,0 +1,284 @@
+local addonName, addon = ...
+local playerIconFiller
+---@type DB
+local db
+---@type CharDB
+local charDb
+-- keep track of stuff we hid to avoid touching things that we didn't change
+local didWeHide = {}
+
+local function ShowHidePlayerCornerIcon()
+	local target = PlayerFrame
+		and PlayerFrame.PlayerFrameContent
+		and PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual
+		and PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PlayerPortraitCornerIcon
+
+	if not target then
+		return
+	end
+
+	local show = type(db.PlayerPortraitCornerIcon) == "boolean" and not db.PlayerPortraitCornerIcon
+
+	if show and not didWeHide["PlayerPortraitCornerIcon"] then
+		return
+	end
+
+	target:SetAlpha(show and 1 or 0)
+
+	-- fill in the hole that's left from hiding the corner icon
+	if not playerIconFiller then
+		playerIconFiller = PlayerFrame:CreateTexture(nil, "ARTWORK")
+		playerIconFiller:SetColorTexture(0, 0, 0, 1)
+		playerIconFiller:SetSize(target:GetWidth() + 2, target:GetHeight() + 2)
+		playerIconFiller:SetPoint("TOPLEFT", target, "TOPLEFT", 0, 0)
+	end
+
+	playerIconFiller:SetAlpha(show and 1 or 0)
+	didWeHide["PlayerPortraitCornerIcon"] = not show
+end
+
+local function ShowHidePrestigeBadge()
+	local playerParent = PlayerFrame
+		and PlayerFrame.PlayerFrameContent
+		and PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual
+	local targetParent = TargetFrame
+		and TargetFrame.TargetFrameContent
+		and TargetFrame.TargetFrameContent.TargetFrameContentContextual
+	local focusParent = FocusFrame
+		and FocusFrame.TargetFrameContent
+		and FocusFrame.TargetFrameContent.TargetFrameContentContextual
+
+	local targets = {
+		playerParent.PrestigeBadge,
+		playerParent.PrestigePortrait,
+
+		targetParent.PrestigeBadge,
+		targetParent.PrestigePortrait,
+
+		focusParent.PrestigeBadge,
+		focusParent.PrestigePortrait,
+	}
+
+	local show = type(db.PrestigeBadges) == "boolean" and not db.PrestigeBadges
+
+	if show and not didWeHide["PrestigeBadges"] then
+		return
+	end
+
+	for _, target in ipairs(targets) do
+		target:SetAlpha(show and 1 or 0)
+	end
+
+	didWeHide["PrestigeBadges"] = not show
+end
+
+local function ShowHideRestingAnimation()
+	local target = PlayerFrame
+		and PlayerFrame.PlayerFrameContent
+		and PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual
+		and PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PlayerRestLoop
+
+	if not target then
+		return
+	end
+
+	local show = type(db.PlayerRestLoop) == "boolean" and not db.PlayerRestLoop
+
+	if show and not didWeHide["PlayerRestLoop"] then
+		return
+	end
+
+	target:SetAlpha(show and 1 or 0)
+
+	didWeHide["PlayerRestLoop"] = not show
+
+	-- hide the flashing animation
+	local statusTexture = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain
+		and PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.StatusTexture
+
+	if statusTexture then
+		if show then
+			statusTexture:Show()
+		else
+			statusTexture:Hide()
+		end
+
+		if not statusTexture.MiniHiderHooked then
+			statusTexture:HookScript("OnShow", function()
+				if not didWeHide["PlayerRestLoop"] then
+					return
+				end
+
+				if db.PlayerRestLoop then
+					statusTexture:Hide()
+				else
+					statusTexture:Show()
+				end
+			end)
+
+			statusTexture.MiniHiderHooked = true
+		end
+	end
+end
+
+local function ShowHideToastButton()
+	local target = QuickJoinToastButton
+
+	if not target then
+		return
+	end
+
+	local show = type(db.QuickJoinToastButton) == "boolean" and not db.QuickJoinToastButton
+
+	if show and not didWeHide["QuickJoinToastButton"] then
+		return
+	end
+
+	target:SetAlpha(show and 1 or 0)
+	didWeHide["QuickJoinToastButton"] = not show
+end
+
+local function ShowHideStanceBar()
+	if not StanceBar then
+		return
+	end
+
+	local show = type(charDb.StanceBar) == "boolean" and not charDb.StanceBar
+
+	if show and not didWeHide["StanceBar"] then
+		return
+	end
+
+	RegisterAttributeDriver(StanceBar, "state-visibility", show and "show" or "hide")
+	didWeHide["StanceBar"] = not show
+end
+
+local function ShowHidePartyTitle()
+	local target = CompactPartyFrameTitle
+
+	if not target then
+		return
+	end
+
+	local show = type(db.CompactPartyFrameTitle) == "boolean" and not db.CompactPartyFrameTitle
+
+	if show and not didWeHide["CompactPartyFrameTitle"] then
+		return
+	end
+
+	target:SetAlpha(show and 1 or 0)
+	didWeHide["CompactPartyFrameTitle"] = not show
+end
+
+local function ShowHideArenaTitle()
+	local target = CompactArenaFrameTitle
+
+	if not target then
+		return
+	end
+
+	if show and not didWeHide["CompactArenaFrameTitle"] then
+		return
+	end
+
+	local show = type(db.CompactArenaFrameTitle) == "boolean" and not db.CompactArenaFrameTitle
+	target:SetAlpha(show and 1 or 0)
+	didWeHide["CompactArenaFrameTitle"] = not show
+end
+
+local function ShowHidePlayerLevel()
+	if not PlayerLevelText then
+		return
+	end
+
+	local show = type(db.PlayerLevelText) == "boolean" and not db.PlayerLevelText
+
+	if show and not didWeHide["PlayerLevelText"] then
+		return
+	end
+
+	-- blizzard expect this function to exists and throws a million errors when it doesn't
+	PlayerLevelText.SetAttribute = PlayerLevelText.SetAttribute or function() end
+	PlayerLevelText:SetAlpha(show and 1 or 0)
+	didWeHide["PlayerLevelText"] = not show
+end
+
+local function ShowHideHotkeys()
+	local show = type(charDb.HotKeysText) == "boolean" and not charDb.HotKeysText
+
+	if show and not didWeHide["HotKeysText"] then
+		return
+	end
+
+	local hotKeyAlpha = show and 1 or 0
+
+	local function ApplyAlpha(frame, alpha)
+		if not frame then
+			return
+		end
+
+		frame:SetAlpha(alpha)
+	end
+
+	for i = 1, 12 do
+		ApplyAlpha(_G["ActionButton" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarBottomLeftButton" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarBottomRightButton" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarRightButton" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarLeftButton" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBar5Button" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBar6Button" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBar7Button" .. i .. "HotKey"], hotKeyAlpha)
+		ApplyAlpha(_G["PetActionButton" .. i .. "HotKey"], hotKeyAlpha)
+
+		ApplyAlpha(_G["ActionButton" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarBottomLeftButton" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarBottomRightButton" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarRightButton" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBarLeftButton" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBar5Button" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBar6Button" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["MultiBar7Button" .. i .. "Name"], hotKeyAlpha)
+		ApplyAlpha(_G["PetActionButton" .. i .. "Name"], hotKeyAlpha)
+	end
+
+	didWeHide["HotKeysText"] = not show
+end
+
+function addon:Run()
+	ShowHideStanceBar()
+	ShowHideRestingAnimation()
+	ShowHidePrestigeBadge()
+	ShowHidePlayerCornerIcon()
+	ShowHidePlayerLevel()
+	ShowHidePartyTitle()
+	ShowHideArenaTitle()
+	ShowHideToastButton()
+	ShowHideHotkeys()
+end
+
+local function OnEvent()
+	-- seems we still need to wait a frame for hotkeys text to load
+	C_Timer.After(0, function()
+		addon:Run()
+	end)
+end
+
+local function Init()
+	addon.Config:Init()
+
+	db = MiniHiderDB or {}
+	charDb = MiniHiderCharDB or {}
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", function(_, event, arg1)
+	if event == "ADDON_LOADED" and arg1 == addonName then
+		Init()
+
+		frame:UnregisterEvent("ADDON_LOADED")
+		frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+		frame:SetScript("OnEvent", OnEvent)
+	end
+end)
